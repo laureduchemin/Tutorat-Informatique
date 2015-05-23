183f57142e0867652d7026886f21470fff8837f8
laureduchemin <duchemin_laure@yahoo.fr>
2015-05-23 18:45:11 +0200

    ajout ressources

---
+++ templates/td-okini/html/com_content/article/default.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+$templateparams = $app->getTemplate(true)->params;
+$images = json_decode($this->item->images);
+$urls = json_decode($this->item->urls);
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+
+// Create shortcut to parameters.
+$params = $this->item->params;
+
+?>
+<article class="item-page<?php echo $this->pageclass_sfx?>">
+<?php if ($this->params->get('show_page_heading')) : ?>
+
+<?php if ($this->params->get('show_page_heading') and $params->get('show_title')) :?>
+<hgroup>
+<?php endif; ?>
+<h1>
+	<?php echo $this->escape($this->params->get('page_heading')); ?>
+</h1>
+<?php endif; ?>
+<?php
+if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->paginationposition && $this->item->paginationrelative)
+{
+	echo $this->item->pagination;
+}
+
+if ($params->get('show_title')) : ?>
+		<h2>
+			<?php echo $this->escape($this->item->title); ?>
+		</h2>
+<?php endif; ?>
+<?php if ($this->params->get('show_page_heading') and $params->get('show_title')) :?>
+</hgroup>
+<?php endif; ?>
+
+<?php if ($params->get('access-edit')) : ?>
+		<ul class="actions">
+		<?php if (!$this->print) : ?>
+				<?php if ($this->user->authorise('core.edit', 'com_content.article.'.$this->item->id)) : ?>
+						<li class="edit-icon">
+							<?php echo JHtml::_('icon.edit', $this->item, $params, array(), true); ?>
+						</li>
+					<?php endif; ?>
+		<?php endif; ?>
+		</ul>
+<?php endif; ?>
+
+	<?php  if (!$params->get('show_intro')) :
+		echo $this->item->event->afterDisplayTitle;
+	endif; ?>
+
+	<?php echo $this->item->event->beforeDisplayContent; ?>
+
+	<?php if (isset ($this->item->toc)) : ?>
+		<?php echo $this->item->toc; ?>
+	<?php endif; ?>
+
+<?php if (isset($urls) AND ((!empty($urls->urls_position) AND ($urls->urls_position == '0')) OR ($params->get('urls_position') == '0' AND empty($urls->urls_position)))
+		OR (empty($urls->urls_position) AND (!$params->get('urls_position')))) : ?>
+
+	<?php echo $this->loadTemplate('links'); ?>
+<?php endif; ?>
+	<?php  if (isset($images->image_fulltext) and !empty($images->image_fulltext)) : ?>
+	<?php $imgfloat = (empty($images->float_fulltext)) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
+
+	<div class="img-fulltext-"<?php echo htmlspecialchars($imgfloat); ?>">
+	<img
+		<?php if ($images->image_fulltext_caption):
+			echo 'class="caption"'.' title="' .htmlspecialchars($images->image_fulltext_caption) .'"';
+		endif; ?>
+		src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"/>
+	</div>
+	<?php endif; ?>
+
+	<?php echo $this->item->text; ?>
+
+	<?php if (isset($urls) AND ((!empty($urls->urls_position) AND ($urls->urls_position == '1')) OR ( $params->get('urls_position') == '1'))) : ?>
+
+	<?php echo $this->loadTemplate('links'); ?>
+	<?php endif; ?>
+
+	<?php echo $this->item->event->afterDisplayContent; ?>
+</article>
+
+
